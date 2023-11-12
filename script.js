371547ffcbdc7fec2c687a34dbd58e3eab7708f4
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-12 14:32:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+function isLeapYear(year){
+
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
+
+function daysInYear(year) {
+  return isLeapYear(year) ? 366 : 365;
+}
+
+console.log(daysInYear(2000));
